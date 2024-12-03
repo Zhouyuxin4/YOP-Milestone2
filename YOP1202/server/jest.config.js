880d7d7d0c135f3f50007c2270d1e6d6afc55248
Xiyu Fan <fanxiyu612@gmail.com>
2024-12-03 14:29:02 -0800

    Update jest.config.js

--- YOP1202/server/jest.config.js
+++ YOP1202/server/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    setupFilesAfterEnv: ['./tests/setup.js'], // 设置全局测试环境
-    testEnvironment: 'node', // 指定测试环境为 Node.js
-    verbose: true, // 输出详细的测试信息
+    setupFilesAfterEnv: ['./tests/setup.js'],
+    testEnvironment: 'node',
+    verbose: true,
 };
-  
